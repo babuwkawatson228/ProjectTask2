4399306268cf5436083e54ab622d0842c2a59dc9
babuwkawatson228 <155854933+babuwkawatson228@users.noreply.github.com>
2026-01-14 14:56:02 +0100

    Update ProductNotFoundException.java

--- src/main/java/pl/edu/vistula/task2/product/support/exception/ProductNotFoundException.java
+++ src/main/java/pl/edu/vistula/task2/product/support/exception/ProductNotFoundException.java
@@ -2,6 +2,7 @@ package pl.edu.vistula.task2.product.support.exception;
 
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long id) {
-        super(String.format("Product with %d not found", id));
+        super(String.format("Product not found", id));
     }
 }
+
